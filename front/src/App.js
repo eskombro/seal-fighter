f73e1c0f48181ca9f0489ce4f500240c03bb70b7
nicolasvienot <nvienot@gmail.com>
2022-06-16 17:02:43 +0200

    Add react-query

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import Home from './pages/Home'
 
+const queryClient = new QueryClient()
+
 const Router = () => (
-  <Routes>
-    <Route path="/" element={<Home />} />
-  </Routes>
+  <QueryClientProvider client={queryClient}>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </QueryClientProvider>
 )
 
 const App = () => <Router />
--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -1,3 +1,7 @@
-const Home = () => <div className="text-9xl text-amber-400">Seal Fighter V</div>
+const Home = () => (
+  <div>
+    <span className="text-9xl text-amber-400">Seal Fighter V</span>
+  </div>
+)
 
 export default Home
