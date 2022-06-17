ed86b9d1594522d339613511f76952717d2535fb
nicolasvienot <nvienot@gmail.com>
2022-06-17 02:31:08 +0200

    Add basic cypress tests for the Info page

--- front/cypress/e2e/basic.cy.js
+++ front/cypress/e2e/basic.cy.js
@@ -1,5 +1,26 @@
-describe('Basic tests', () => {
+describe('Basic Home page tests', () => {
   it('Should visit /', () => {
     cy.visit('/')
+  })
+})
+
+describe('Basic Info page tests', () => {
+  it('Should visit /seals/george', () => {
+    cy.visit('/seals/george')
+  })
+
+  it('Should display George HP', () => {
+    cy.visit('/seals/george')
+    cy.contains('HP: 1000').should('be.visible')
+  })
+
+  it('Should display George Attack', () => {
+    cy.visit('/seals/george')
+    cy.contains('ATTACK: 30').should('be.visible')
+  })
+
+  it('Should display George Defense', () => {
+    cy.visit('/seals/george')
+    cy.contains('DEFENSE: 30').should('be.visible')
   })
 })
