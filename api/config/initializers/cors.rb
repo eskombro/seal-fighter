6e7eedbb3bbb718b73562eec60252c0767de5cc8
nicolasvienot <nvienot@gmail.com>
2022-06-21 15:12:48 +0200

    Fix linter in api

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -10,7 +10,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins 'localhost:3000', '127.0.0.1:3000'
 
     resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+             headers: :any,
+             methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
